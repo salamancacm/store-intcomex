96cc246bbf257444f44f9e1205a95bd19dc47134
Carlos Salamanca <salamancacmcol@gmail.com>
2022-10-16 15:06:15 -0500

    Import

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+USE store_intcomex;
+
+INSERT INTO categories (category_id, category_name,description,picture) VALUES (1, "SERVIDORES", "Categoria de servidores","https://concepto.de/wp-content/uploads/2019/05/servidor-e1557165670831.jpg");
+INSERT INTO categories (category_id, category_name,description,picture) VALUES (2, "CLOUD", "Categoria de cloud","https://ava-inc.com/wp-content/uploads/2018/12/move-your-small-business-to-the-cloud.jpg");
+
+INSERT INTO suppliers (supplier_id, address, city, company_name, contact_name, contact_title, country, fax, home_page, phone, postal_code, region) VALUES (1, "Av. 1 con calle 2", "Google Inc", "San Diego", "Sheldon Cooper", "Mr.", "USA", 1, "homePage", "+123321233232", "", "California");
