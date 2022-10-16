c8fd7463d4821b4ec322d69e33f850fc7a381dab
Carlos Salamanca <salamancacmcol@gmail.com>
2022-10-16 14:11:26 -0500

    Change of eureka url

--- src/main/java/com/intcomex/store/StoreApplication.java
+++ src/main/java/com/intcomex/store/StoreApplication.java
@@ -2,9 +2,11 @@ package com.intcomex.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StoreApplication {
 
 	public static void main(String[] args) {
